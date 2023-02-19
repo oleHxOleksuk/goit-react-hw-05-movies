9bc23c3a7c5edf292c52c7337e9584da26cc84c2
oleHxOleksuk <108345191+oleHxOleksuk@users.noreply.github.com>
2023-02-19 16:45:30 +0200

    ht45ui5

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,18 +1,18 @@
 import { Routes, Route } from "react-router-dom";
 import { lazy, Suspense } from "react";
 
-import GlobalStyle from 'globalStyles';
+import GlobalStyle from 'styled-components';
 
-import SharedLayout from 'Shared/SharedLayout/SharedLayout'
+import SharedLayout from '../Shared/SharedLayout/SharedLayout'
 import Loader from "Shared/Loader/Loader";
 
-const Home = lazy(() => import('./pages/Home/Home'));
+const Home = lazy(() => import('./pages/HomePage/Home'));
 const Movies = lazy(() => import('./pages/MoviesPage/Movies'));
 const NotFoundPage = lazy(() => import('./pages/NotFoundPage/NotFoundPage'));
 const MoviesInformation = lazy(() => import('./pages/MoviesInformation/MoviesInformations'));
 
-const Cast = lazy(() => import('./components/Cast/Cast'));
-const MovieRewiewers = lazy(() => import('./components/MovieRewiewers/MovieRewiewers'));
+const Cast = lazy(() => import('./Cast/Cast'));
+const MovieRewiewers = lazy(() => import('./MovieRewiewers/MovieRewiewers'));
 
 
 
@@ -26,9 +26,9 @@ export const App = () => {
       <Routes>
       <Route path="/" element={<SharedLayout />}>
         <Route index element={<Home />} />
-        <Route path="/movies" element={<Movies />} />
-        <Route path="/movies/:movieId" element={<MoviesInformation />}>
-          <Route path="cast" element={<Cast />} />
+        <Route path="/movies" element={<Movies/>} />
+        <Route path="/movies/:movieId" element={<MoviesInformation/>}>
+          <Route path="cast" element={<Cast/>} />
           <Route path="reviews" element={<MovieRewiewers />} />
         </Route>
         <Route path="*" element={<NotFoundPage />} />
